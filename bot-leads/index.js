9122f8902dbd1386e7b61fa437064c427b3087e8
Wellyngton Oliveira <wellyngtonsabinocoelho@gmail.com>
2025-09-19 14:37:33 -0300

    Convertendo index.js para CommonJS (require)

--- bot-leads/index.js
+++ bot-leads/index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
